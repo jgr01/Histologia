fa55de98fb77f56b0ea0f6f98038b1c341b3d2d1
xp <xp@Android004>
2016-10-23 21:33:49 +0200

    adivina

--- adivina_quien_soy.php
+++ adivina_quien_soy.php
@@ -1,21 +1,7 @@
 <?php
-
-    $puntuacion = 0;
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') 
-    {
-        if ($_POST['imagenCargada'] == $_POST['respuesta'])
-        {
-            echo "CORRECTO";
-            
-            $puntuacion = $puntuacion + 1 ;
-            
-        }
-        else 
-        {
-            echo "INCORRECTO";
-            
-        }
-    }
+$acertado = '<img src="img\acertado.jpg" style="width: 50px; height: 50px;"/>';
+$fallado = '<img src="img\fallado.jpg" style="width: 50px; height: 50px;"/>';
+$puntuacion = 0;
 ?>
 
 <html>
@@ -23,7 +9,7 @@
         <meta charset="UTF-8">
         <title>Prueba de PHP</title>
         <link rel="stylesheet" href="css/bootstrap.min.css">
-        
+
         <style>
             #body{
                 background-color: #a6e1ec;
@@ -36,161 +22,248 @@
                 margin-bottom: 15px;
                 border: solid;
             }
-            
- 
-</style>
-    
+            img{
+                width:100%;
+                /*max-width:600px;*/
+            }
+            .pot{
+                text-align: center;
+
+            }
+            .letra{
+                font-size: large;
+                font-style: italic
+            }
+
+
+        </style>
+
     </head>
     <body id="body">
-    <div class="container, margenes">
-        <div class="row">
-            <div class="col-md-3">
-            
-        </div>
-        <form method="POST" action="adivina_quien_soy.php">
-            <div class="col-md-3" >
-                <img class="margenfoto" src="img\imagen0020.jpg" style="width: 300px; height: 300px;"/>
+        <br>
+        <div class="container, margenes">
+            <div class="row">
+                <button class="btn btn-block btn-info disabled" style="font-size: large;"><h2>Seleccione la respuesta que corresponde con la imagen:</h2></button>
+                <br>
+                <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3">
+
+                </div>
+                <form method="POST" action="adivina_quien_soy.php">
+                    <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3" >
+                        <img class="margenfoto" src="img\imagen0020.jpg"/>
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3"><br><br><br>
+
+                        <input id="respuesta1" name="respuesta1" type="radio" value="10"  /><span class="letra"> Mitocondria</span>
+                        <br /><br />
+                        <input id="respuesta1" name="respuesta1" type="radio" value="20" /><span class="letra"> Célula</span>
+                        <br /><br />
+                        <input id="respuesta1" name="respuesta1" type="radio" value="30" /><span class="letra"> Bacteria</span>
+                        <br /><br /> 
+                        <input id="respuesta1" name="respuesta1" type="radio" value="40" /><span class="letra"> Tejido</span>
+                        <br /><br />
+                        <input id="respuesta1" name="respuesta1" type="radio" value="50" /><span class="letra"> Músculo</span>
+                        <br />
+                        <br />
+                        <input type="hidden" id="imagenCargada1" name="imagenCargada1" value="20" />
+                        <!--<input type="submit" value="Comprobar" />-->
+
+                    </div>
+
+                    <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3"><br><br><br><br><br>
+                        <?php
+                        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+                            if (isset($_POST['respuesta1'])== TRUE) {
+                                if ($_POST['imagenCargada1'] == $_POST['respuesta1']) {
+                                    //echo "CORRECTO";
+                                    echo $acertado;
+                                    $puntuacion = $puntuacion + 1;
+                                } else {
+                                    //echo "INCORRECTO";
+                                    echo $fallado;
+                                }
+                            }
+                            else {
+                            echo $fallado;
+                        }
+                        }
+//            echo $puntuacion 
+                        ?>
+
+                    </div>
             </div>
-            <div class="col-md-3" >
+            <div class="row">
+                <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3">
 
-                Seleccione la respuesta que corresponde con la imagen:
+                </div>
+                <!--        <form method="POST2" action="adivina_quien_soy.php">-->
+                <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3" >
+                    <img class="margenfoto" src="img\imagen0021.jpg"/>
+                </div>
+                <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3" >
 
-                <br/>
 
-                <br />
-                <input id="respuesta" name="respuesta" type="radio" value="10" />Mitocondria
-                <br /><br />
-                <input id="respuesta" name="respuesta" type="radio" value="20" />Célula
-                <br /><br />
-                <input id="respuesta" name="respuesta" type="radio" value="30" />Bacteria
-                <br /><br /> 
-                <input id="respuesta" name="respuesta" type="radio" value="40" />Tejido
-                <br /><br />
-                <input id="respuesta" name="respuesta" type="radio" value="50" />Músculo
-                <br />
-                <br />
-                <input type="hidden" id="imagenCargada" name="imagenCargada" value="20" />
-                <input type="submit" value="Comprobar" />
-                
+                    <br/>
+
+                    <br />
+                    <input id="respuesta2" name="respuesta2" type="radio" value="10" /><span class="letra"> Mitocondria</span>
+                    <br /><br />
+                    <input id="respuesta2" name="respuesta2" type="radio" value="20" /><span class="letra"> Célula</span>
+                    <br /><br />
+                    <input id="respuesta2" name="respuesta2" type="radio" value="30" /><span class="letra"> Bacteria</span>
+                    <br /><br /> 
+                    <input id="respuesta2" name="respuesta2" type="radio" value="40" /><span class="letra"> Tejido</span>
+                    <br /><br />
+                    <input id="respuesta2" name="respuesta2" type="radio" value="50" /><span class="letra"> Músculo</span>
+                    <br />
+                    <br />
+                    <input type="hidden" id="imagenCargada2" name="imagenCargada2" value="30" />
+                    <!--<input type="submit" value="Comprobar" />-->
+
+                </div>
+                <!--</form>-->
+                <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3"><br><br><br><br><br>
+                    <?php
+                    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                        if (isset($_POST['respuesta1']) == TRUE) {
+                            if ($_POST['imagenCargada2'] == $_POST['respuesta2']) {
+                                //echo "CORRECTO";
+                                echo $acertado;
+                                $puntuacion = $puntuacion + 1;
+                            } else {
+                                //echo "INCORRECTO";
+                                echo $fallado;
+                            }
+                        }
+                        else {
+                            echo $fallado;
+                    }
+                    }
+//            echo $puntuacion 
+                    ?>
+
+                </div>
             </div>
-        </form>
-        <div class="col-md-3">
-            <h3>Puntuación <?php echo $puntuacion ?></h3>
-        </div>
-</div>
-        <div class="row">
-            <div class="col-md-3">
-            
-        </div>
-        <form method="POST" action="adivina_quien_soy.php">
-            <div class="col-md-3">
-                <img class="margenfoto" src="img\imagen0021.jpg" style="width: 300px; height: 300px;"/>
+            <div class="row">
+                <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3">
+
+                </div>
+                <!--<form method="POST" action="adivina_quien_soy.php">-->
+                <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3" >
+                    <img class="margenfoto" src="img\imagen0022.jpg"/>
+                </div>
+                <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3" >
+
+
+
+                    <br/>
+
+                    <br />
+                    <input id="respuesta3" name="respuesta3" type="radio" value="10" /><span class="letra"> Mitocondria</span>
+                    <br /><br />
+                    <input id="respuesta3" name="respuesta3" type="radio" value="20" /><span class="letra"> Célula</span>
+                    <br /><br />
+                    <input id="respuesta3" name="respuesta3" type="radio" value="30" /><span class="letra"> Bacteria</span>
+                    <br /><br /> 
+                    <input id="respuesta3" name="respuesta3" type="radio" value="40" /><span class="letra"> Tejido</span>
+                    <br /><br />
+                    <input id="respuesta3" name="respuesta3" type="radio" value="50" /><span class="letra"> Músculo</span>
+                    <br />
+                    <br />
+                    <input type="hidden" id="imagenCargada3" name="imagenCargada3" value="40" />
+                    <!--<input type="submit" value="Comprobar" />-->
+
+                </div>
+                <!--</form>-->
+                <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3"><br><br><br><br><br>
+                    <?php
+                    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                        if (isset($_POST['respuesta1']) == TRUE) {
+                            if ($_POST['imagenCargada3'] == $_POST['respuesta3']) {
+                                //echo "CORRECTO";
+                                echo $acertado;
+                                $puntuacion = $puntuacion + 1;
+                            } else {
+                                //echo "INCORRECTO";
+                                echo $fallado;
+                            }
+                        }
+                        else {
+                            echo $fallado;
+                    }
+                    }
+//            echo $puntuacion 
+                    ?>
+                </div>
             </div>
-            <div class="col-md-3" >
+            <div class="row">
+                <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3">
 
-                Seleccione la respuesta que corresponde con la imagen:
+                </div>
+                <!--<form method="POST" action="adivina_quien_soy.php">-->
+                <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3" >
+                    <img class="margenfoto" src="img\imagen0023.jpg"/><br><br>
 
-                <br/>
+                </div>
+                <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3" >
 
-                <br />
-                <input id="respuesta" name="respuesta" type="radio" value="10" />Mitocondria
-                <br /><br />
-                <input id="respuesta" name="respuesta" type="radio" value="20" />Célula
-                <br /><br />
-                <input id="respuesta" name="respuesta" type="radio" value="30" />Bacteria
-                <br /><br /> 
-                <input id="respuesta" name="respuesta" type="radio" value="40" />Tejido
-                <br /><br />
-                <input id="respuesta" name="respuesta" type="radio" value="50" />Músculo
-                <br />
-                <br />
-                <input type="hidden" id="imagenCargada" name="imagenCargada" value="20" />
-                <input type="submit" value="Guardar" />
-                
+
+
+                    <br/>
+
+                    <br />
+                    <input id="respuesta4" name="respuesta4" type="radio" value="10" /><span class="letra"> Mitocondria</span>
+                    <br /><br />
+                    <input id="respuesta4" name="respuesta4" type="radio" value="20" /><span class="letra"> Célula</span>
+                    <br /><br />
+                    <input id="respuesta4" name="respuesta4" type="radio" value="30" /><span class="letra"> Bacteria</span>
+                    <br /><br /> 
+                    <input id="respuesta4" name="respuesta4" type="radio" value="40" /><span class="letra"> Tejido</span>
+                    <br /><br />
+                    <input id="respuesta4" name="respuesta4" type="radio" value="50" /><span class="letra"> Músculo</span>
+                    <br />
+                    <br />
+                    <input type="hidden" id="imagenCargada4" name="imagenCargada4" value="50" /><br><br>
+
+
+                </div>
+
+                <!--                </form>-->
+                <!--</form>-->
+                <div class="col-md-3 col-lg-3 col-sm-3 col-xs-3"><br><br><br><br><br>
+
+
+
+                    <?php
+                    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                        if (isset($_POST['respuesta1']) == TRUE) {
+                            if ($_POST['imagenCargada4'] == $_POST['respuesta4']) {
+                                //echo "CORRECTO";
+                                echo $acertado;
+                                $puntuacion = $puntuacion + 1;
+                            } else {
+                                //echo "INCORRECTO";
+                                echo $fallado;
+                            }
+                        } else {
+                            echo $fallado;
+                        }
+                    }
+//            echo $puntuacion 
+                    ?>
+                </div>
             </div>
-        </form>
-        <div class="col-md-3">
-           
         </div>
-</div>
-        <div class="row">
-            <div class="col-md-3">
-            
+        <div style="text-align: center; font-size: 30px ">  
+            <input class="bottom btn-success" type="submit" value="Comprobar" style="border-bottom-color: #0f0f0f; font-size: larger; text-align: center;" /><br><br>
+
         </div>
-        <form method="POST" action="adivina_quien_soy.php">
-            <div class="col-md-3">
-                <img class="margenfoto" src="img\imagen0022.jpg" style="width: 300px; height: 300px;"/>
-            </div>
-            <div class="col-md-3" >
+    </form>
 
-                Seleccione la respuesta que corresponde con la imagen:
 
-                <br/>
-
-                <br />
-                <input id="respuesta" name="respuesta" type="radio" value="10" />Mitocondria
-                <br /><br />
-                <input id="respuesta" name="respuesta" type="radio" value="20" />Célula
-                <br /><br />
-                <input id="respuesta" name="respuesta" type="radio" value="30" />Bacteria
-                <br /><br /> 
-                <input id="respuesta" name="respuesta" type="radio" value="40" />Tejido
-                <br /><br />
-                <input id="respuesta" name="respuesta" type="radio" value="50" />Músculo
-                <br />
-                <br />
-                <input type="hidden" id="imagenCargada" name="imagenCargada" value="20" />
-                <input type="submit" value="Guardar" />
-                
-            </div>
-        </form>
-        <div class="col-md-3">
-           
-        </div>
-</div>
-        <div class="row">
-            <div class="col-md-3">
-            
-        </div>
-        <form method="POST" action="adivina_quien_soy.php">
-            <div class="col-md-3">
-                <img class="margenfoto" src="img\imagen0023.jpg" style="width: 300px; height: 300px;"/>
-            </div>
-            <div class="col-md-3" >
-
-                Seleccione la respuesta que corresponde con la imagen:
-
-                <br/>
-
-                <br />
-                <input id="respuesta" name="respuesta" type="radio" value="10" />Mitocondria
-                <br /><br />
-                <input id="respuesta" name="respuesta" type="radio" value="20" />Célula
-                <br /><br />
-                <input id="respuesta" name="respuesta" type="radio" value="30" />Bacteria
-                <br /><br /> 
-                <input id="respuesta" name="respuesta" type="radio" value="40" />Tejido
-                <br /><br />
-                <input id="respuesta" name="respuesta" type="radio" value="50" />Músculo
-                <br />
-                <br />
-                <input type="hidden" id="imagenCargada" name="imagenCargada" value="20" />
-                <input type="submit" value="Guardar" />
-                
-            </div>
-        </form>
-        <div class="col-md-3">
-           
-        </div>
-</div>
-    </div>
-        
-        
-          
-       
-        </body>
-        <?php
-
+</body>
+<?php
 //    $pregunta1 = $_POST['imagen'];
 //    $pregunta2 = $_POST['imagen1'];
 //    $pregunta3 = $_POST['imagen2'];
@@ -205,6 +278,5 @@
 //    if ($answer5) { $totalCorrect++; }
 //
 //    echo "<div id='results'>$totalCorrect / 5 correct</div>";
-
 ?>
 </html>
--- de_donde_vienes.php
+++ de_donde_vienes.php
@@ -9,6 +9,7 @@
         <style>
             body{
                 text-align: center;
+                background-color: #c4e3f3;
             }
             /*Creacion de los 2 elementos*/
             .drag {
@@ -18,11 +19,14 @@
                 -webkit-border-radius: 50%;
                 border-radius: 50%;
                 background: #3498db;
-                /*margin: 1.5em auto 0;*/
+                border-style: ridge;
+                padding: 10px
+                /*margin: 1.5em auo 0;*/
             }
             .drop1 {
                 width: 100%;
                  height: 200px;
+                
                  /*
                  -moz-border-radius: 50%;
                  -webkit-border-radius: 50%;
@@ -41,23 +45,29 @@
     </head>
     <body>
         <div class="container">
-
-            <h1><p>Arrastrar y Soltar</p></h1><br>
+            <br>
+            <button class="btn btn-block btn-info disabled" style="font-size: large;"><h2>Arrastra y Suetla donde corresponda</h2></button>
+                <br>
             <div class="row">
                 <div class="col-md-4 col-lg-4 col-xs-4 col-sm-4" align = "center">
-                    <div class="drag" id="drag1" draggable="true">Célula</div><br>
+<!--                    <div class="drag" id="drag1" draggable="true">Célula</div><br>
                     <div class="drag" id="drag2" draggable="true">Mitocondria</div><br>
                     <div class="drag" id="drag3" draggable="true">Músculo</div><br>
-                    <div class="drag" id="drag4" draggable="true">Bacteria</div><br>
+                    <div class="drag" id="drag4" draggable="true">Bacteria</div><br>-->
+                    <br><br>
+                    <div class="drag" id="drag1" draggable="true" >Célula</div><br><br><br>
+                    <div class="drag" id="drag4" draggable="true" >Mitocondria</div><br><br><br>
+                    <div class="drag" id="drag3" draggable="true" >Músculo</div><br><br><br>
+                    <div class="drag" id="drag2" draggable="true" >Bacteria</div><br><br><br>
                 </div>
 
                 <div class="col-md-3 col-lg-3 col-xs-3 col-sm-3"></div>
 
                 <div class="col-md-4 col-lg-4 col-xs-4 col-sm-4" align = "center">
-                    <div class="drop1" id="drop3" style="background:url('img/imagen0020.jpg')"></div><br>
-                    <div class="drop1" id="drop2">2</div><br>
-                    <div class="drop1" id="drop1">1</div><br>
-                    <div class="drop1" id="drop4">4</div><br>
+                    <div class="drop1" id="drop3" style="background:url('img/imagen0020.jpg'); background-size: contain; zoom: 80%; background-repeat: no-repeat"></div><br>
+                    <div class="drop1" id="drop2" style="background:url('img/imagen0021.jpg'); background-size: contain; zoom: 80%; background-repeat: no-repeat"></div><br>
+                    <div class="drop1" id="drop1" style="background:url('img/imagen0022.jpg'); background-size: contain; zoom: 80%; background-repeat: no-repeat"></div><br>
+                    <div class="drop1" id="drop4" style="background:url('img/imagen0023.jpg'); background-size: contain; zoom: 80%; background-repeat: no-repeat"></div><br>
                 </div>
 
                 <div class="col-md-1 col-lg-1 col-xs-1 col-sm-1"></div>
@@ -95,6 +105,8 @@
                     e.target.appendChild(document.getElementById(id));
                 }
             }
+            
+            
         }
         window.onload = function () {
             DragDrop("drag1", "drop1");
